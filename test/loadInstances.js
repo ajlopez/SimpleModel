160c46d357693bc2aa361a65443bfab530c50734
ajlopez <webmaster@ajlopez.com>
2012-05-30 04:58:47 -0300

    loadInstances in entity with tests

---
+++ test/loadInstances.js
@@ -0,0 +1,15 @@
+
+var $ = require('../'),
+	assert = require('assert'),
+	path = require('path');
+	
+var model = $.loadModel(path.join(__dirname, 'model.json'));
+var entity = model.entities.name('customer');
+
+entity.loadInstances(path.join(__dirname, 'customers.json'));
+
+assert.ok(entity.instances());
+assert.ok(entity.instances().id(1));
+assert.equal(entity.instances().id(2).name, 'Apple');
+assert.equal(entity.instances().name('Google').id, 3);
+
