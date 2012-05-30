02fca3828400de6c2f5928d00adf79cdd44e5882
ajlopez <webmaster@ajlopez.com>
2012-05-30 04:51:17 -0300

    getByName, getById refactor to name, id

--- test/loadModel.js
+++ test/loadModel.js
@@ -7,9 +7,9 @@ var model = $.loadModel(path.join(__dirname, 'model.json'));
 
 assert.ok(model);
 assert.ok(model.entities);
-assert.ok(model.entities.getByName('customer'));
-assert.ok(model.entities.getByName('order'));
-assert.equal(model.entities.getByName('foo'), null);
+assert.ok(model.entities.name('customer'));
+assert.ok(model.entities.name('order'));
+assert.equal(model.entities.name('foo'), null);
 assert.ok(model.entities.where(function(item) { return item.name == 'customer'; }));
 assert.equal(model.entities.where(function(item) { return item.name == 'customer'; }).length, 1);
 
